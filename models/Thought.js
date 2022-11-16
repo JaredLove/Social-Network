9f34cac9553e80bd86816b2f98a55a237d915d3d
Jared Love <lovejared91@yahoo.com>
2022-11-16 15:06:33 -0600

    added date format for Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -20,7 +20,7 @@ const reactionSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now,
-        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        get: createdAtVal => dateFormat(createdAtVal)
     }
 },
     {
